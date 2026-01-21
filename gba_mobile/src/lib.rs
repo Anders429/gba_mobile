421c4abb9fa089cac046cf945ff4e0cbb0431509
Anders429 <anders.stefan.evensen@gmail.com>
2026-01-20 23:24:58 -0700

    Initial commit.

---
+++ gba_mobile/src/lib.rs
@@ -0,0 +1,12 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![cfg_attr(test, feature(custom_test_frameworks))]
+#![cfg_attr(test, test_runner(gba_test::runner))]
+#![cfg_attr(test, reexport_test_harness_main = "test_harness")]
+
+#[cfg(test)]
+#[unsafe(no_mangle)]
+pub fn main() {
+    let _ = mgba_log::init();
+    test_harness()
+}
