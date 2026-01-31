8429545bf1bd5ed088a4e66206eb66840cb1b178
Anders429 <anders.stefan.evensen@gmail.com>
2026-01-31 09:28:51 -0700

    Implement communication for idle state.

--- gba_mobile/src/engine/request/mod.rs
+++ gba_mobile/src/engine/request/mod.rs
@@ -7,13 +7,20 @@ pub(in crate::engine) use packet::Packet;
 
 use crate::{
     engine::{Adapter, Source},
-    mmio::serial::TransferLength,
+    mmio::serial::{SIODATA8, SIODATA32, TransferLength},
 };
+
+const FRAMES_100_MILLISECONDS: u8 = 7;
+const FRAMES_3_SECONDS: u8 = 180;
 
 #[derive(Debug)]
 pub(in crate::engine) enum Request {
     Packet(Packet),
-    WaitForIdle,
+    WaitForIdle {
+        // TODO: Separate this all out into another module?
+        transfer_length: TransferLength,
+        frame: u8,
+    },
 }
 
 impl Request {
@@ -21,14 +28,33 @@ impl Request {
         Self::Packet(Packet::new(transfer_length, source))
     }
 
+    pub(in crate::engine) fn new_wait_for_idle(transfer_length: TransferLength) -> Self {
+        Self::WaitForIdle {frame: 0, transfer_length}
+    }
+
     pub(in crate::engine) fn vblank(&mut self) {
-        todo!()
+        match self {
+            Self::Packet(_) => todo!("timeouts?"),
+            Self::WaitForIdle {frame, transfer_length} => {
+                if *frame % FRAMES_100_MILLISECONDS == 0 {
+                    // Send a new idle byte.
+                    match transfer_length {
+                        TransferLength::_8Bit => unsafe{ SIODATA8.write_volatile(0x4b)},
+                        TransferLength::_32Bit => unsafe {SIODATA32.write_volatile(0x4b_4b_4b_4b);}
+                    }
+                }
+                if *frame > FRAMES_3_SECONDS {
+                    todo!("timeout")
+                }
+                *frame += 1;
+            }
+        }
     }
 
     pub(in crate::engine) fn timer(&mut self) {
         match self {
             Self::Packet(packet) => packet.pull(),
-            Self::WaitForIdle => {}
+            Self::WaitForIdle {..} => {}
         }
     }
 
@@ -38,7 +64,24 @@ impl Request {
                 .push(adapter)
                 .map(|next_packet| next_packet.map(Self::Packet))
                 .map_err(Error::Packet),
-            Self::WaitForIdle => todo!(),
+            Self::WaitForIdle {transfer_length, ..} => {
+                match transfer_length {
+                    TransferLength::_8Bit => {
+                        if unsafe {SIODATA8.read_volatile()} == 0xd2 {
+                            Ok(None)
+                        } else {
+                            Ok(Some(self))
+                        }
+                    }
+                    TransferLength::_32Bit => {
+                        if unsafe {SIODATA32.read_volatile()} == 0xd2_d2_d2_d2 {
+                            Ok(None)
+                        } else {
+                            Ok(Some(self))
+                        }
+                    }
+                }
+            }
         }
     }
 }
